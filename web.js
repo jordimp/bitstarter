7408dfa12205d7284934700692b237c5746f63bc
Jordi Martin <jmartin@digitalchocolate.com>
2013-07-10 15:57:31 +0200

    Reads hello world from index.html file

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+// cache file contents
+var indexHtmlStr = fs.readFileSync("index.html").toString("utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(indexHtmlStr);
 });
 
 var port = process.env.PORT || 5000;
